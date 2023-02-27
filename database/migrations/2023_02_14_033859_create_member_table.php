e33e0bd8a76687cb28e094d8b6ab686653a8d10c
Agatha <rikilesmana2304@gmail.com>
2023-02-27 12:06:18 +0700

    Commit All

---
+++ database/migrations/2023_02_14_033859_create_member_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+            $table->id();
+            $table->char('nama', 100);
+            $table->text('text');
+            $table->enum('jenis_kalamin', ['L','P']);
+            $table->char('tlp');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\BarangController;
-use App\Http\Controllers\PemasokController;
-use App\Http\Controllers\ProdukController;
-use App\Http\Controllers\PembelianController;
-use App\Http\Controllers\ContohController;
-
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,13 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', HomeController::class);
-Route::resource('/barang', BarangController::class);
-Route::resource('/produk', ProdukController::class);
-Route::resource('/pemasok', PemasokController::class);
-Route::resource('/pembelian', PembelianController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
